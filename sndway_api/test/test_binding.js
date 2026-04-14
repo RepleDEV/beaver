27c7d502d74f4f79d88774e73f24e736588799b6
RepleDEV <ragazzochaesa@gmail.com>
2026-04-14 15:10:45 +0700

    test(sndway_api): add proper tests

--- sndway_api/test/test_binding.js
+++ sndway_api/test/test_binding.js
@@ -1,20 +1,58 @@
 const SndwayApi = require("../dist/binding.js");
 const assert = require("assert");
+const { describe, it, before } = require("node:test");
+const readline = require("node:readline/promises")
 
-assert(SndwayApi, "The expected module is undefined");
+describe("Tests", () => {
+    isDeviceConnected = false;
 
-function testBasic() {
+    before(async () => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        const ans = await rl.question("Is the device connected? (Y/N)");
+
+        if (ans == "Y") {
+            isDeviceConnected = true
+            return
+        }
+    });
+
     const instance = new SndwayApi();
-    const res = instance.open();
 
-    if (res == 1) {
-        console.log("Device did not open")
-        return;
+    it("Should match device connection status", () => {
+        const res = instance.open();
+
+        assert.strictEqual(instance.is_connected(), isDeviceConnected)
+    });
+
+    if (isDeviceConnected) {
+        it("Should give a reading", () => {
+            const reading = instance.read();
+            const now = +(new Date());
+
+            console.log(`Reading: ${reading.reading}`);
+            console.log(`Timestamp delta: ${now - reading.timestamp} ms`);
+
+            assert.notEqual(reading.reading, 0);
+        });
     }
+});
 
-    console.log(`Reading: ${JSON.stringify(instance.read())}`);
-}
-
-assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
-
-console.log("Tests passed- everything looks OK!");
+// function testBasic() {
+//     const instance = new SndwayApi();
+//     const res = instance.open();
+//
+//     if (res == 1) {
+//         console.log("Device did not open")
+//         return;
+//     }
+//
+//     console.log(`Reading: ${JSON.stringify(instance.read())}`);
+// }
+//
+// assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
+//
+// console.log("Tests passed- everything looks OK!");
